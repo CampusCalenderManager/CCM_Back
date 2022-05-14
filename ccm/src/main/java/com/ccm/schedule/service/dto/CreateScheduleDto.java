2dfbe76be11651a3661457109b67ec7c076ffe85
Dongun Shin <huipulci1@naver.com>
2022-05-14 11:09:09 +0900

    Add Schedule (#6)
    Co-authored-by: VertexToEdge <vertextoedge@gmail.com>

---
+++ ccm/src/main/java/com/ccm/schedule/service/dto/CreateScheduleDto.java
@@ -0,0 +1,30 @@
+/*
+ * Created by VertexToEdge on 2022/5/10.
+ */
+
+package com.ccm.schedule.service.dto;
+
+import com.ccm.member.domain.Member;
+import com.ccm.schedule.domain.Schedule;
+
+import java.time.LocalDateTime;
+
+public class CreateScheduleDto {
+    private String title;
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    private LocalDateTime startAlarm;
+    private LocalDateTime endAlarm;
+
+    private boolean isShared;
+
+    private String color;
+
+    private Long memberId;
+
+    public Schedule toEntity() {
+        return Schedule.builder().title(title).startDate(startDate).endDate(endDate).startAlarm(startAlarm).endAlarm(endAlarm).isShared(isShared).color(color).member(new Member(memberId)).build();
+    }
+}
---
+++ ccm/src/main/java/com/ccm/schedule/service/dto/ScheduleSearchCond.java
@@ -0,0 +1,8 @@
+/*
+ * Created by VertexToEdge on 2022/5/10.
+ */
+
+package com.ccm.schedule.service.dto;
+
+public class ScheduleSearchCond {
+}
