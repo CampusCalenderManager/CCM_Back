53df7d6ee72d546bf7b7a4421de0253b23d23016
ShinDongHun1 <huipulci1@naver.com>
2022-05-26 03:20:21 +0900

    [fix] 그룹 생성 시 참여코드 반환할 때 필드명 수정

--- ccm/src/main/java/com/ccm/organization/controller/dto/ParticipationCodeResponse.java
+++ ccm/src/main/java/com/ccm/organization/controller/dto/ParticipationCodeResponse.java
@@ -5,7 +5,7 @@ import com.ccm.organization.domain.ParticipationCode;
 /**
  * Created by ShinD on 2022/05/26.
  */
-public record ParticipationCodeResponse(String code) {
+public record ParticipationCodeResponse(String participationCodeResponse) {
 	public static ParticipationCodeResponse from(ParticipationCode participationCode) {
 		return new ParticipationCodeResponse(participationCode.getCode());
 	}
